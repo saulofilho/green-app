8667c03dedddd5e6a8191fa8d03537b8e60cc76e
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-04-27 11:10:57 -0300

    [chore] url prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const api = axios.create({ baseURL: 'https://buenavistalabdatacenter.com/' });
-const api = axios.create({ baseURL: 'http://localhost:3333/' });
+const api = axios.create({ baseURL: 'https://buenavistalabdatacenter.com/' });
+// const api = axios.create({ baseURL: 'http://localhost:3333/' });
 
 export default api;
