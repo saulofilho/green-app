5d89b9e0dfdfa128953bf40dd75578763c1c00e6
saulofilho <saulosilvaf@gmail.com>
2022-12-20 18:57:40 -0300

    remove register

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-// import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
 import Project from '../pages/Project';
 import Profile from '../pages/Profile';
@@ -13,7 +12,6 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
-      {/* <Route path="/register" exact component={Register} isPrivate /> */}
       <Route path="/dashboard" exact component={Dashboard} isPrivate />
       <Route path="/project/:id" exact component={Project} isPrivate />
       <Route path="/profile" exact component={Profile} isPrivate />
